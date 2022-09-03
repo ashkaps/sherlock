96f52c9d08304a8da2da3b8c059d3fa21d3032f9
AK <31744402+ashkaps@users.noreply.github.com>
2022-09-03 21:38:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from flask import Flask, request
 import similarity
 import traceback
-app = Flask(__name__)
 
-
-@app.route('/check',methods = ['POST'])
-def result():
-   if request.method == 'POST':
-      try:
-         result = None
-         if request.json is not None:
-            result = request.json.get('text', None)
-         if result is None and request.form is not None:
-            result = request.form.get('text', None)
-         if result is None:
-            return {'message':"text not sent"}, 400
-         report_json  = similarity.report(str(result))
-         response = {
-            'text': result,
-            'data': report_json
-         }
-         return response, 200
-      except Exception as e:
-         return {'message':traceback.format_exc()}, 500
-
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080)
+def check(text):
+   '''
+   '''
+   try:
+      if text is None:
+         return {'message':"text not sent"}, 400
+      report_json  = similarity.report(str(text))
+      response = {
+         'text': text,
+         'data': report_json
+      }
+      return response, 200
+   except Exception as e:
+      return {'message':traceback.format_exc()}, 500
